9856c40299bde096d14f9036051edf5f747dee21
naman.jain <naman.jain@media.net>
2025-08-16 00:25:57 +0530

    Added some files

--- typescript-migration/test/test_fragmentURI.ts
+++ typescript-migration/test/test_fragmentURI.ts
@@ -6,15 +6,14 @@
 
 declare var URI: any;
 
-var moduleFn = (globalThis as any).QUnit.module;
-var testFn = (globalThis as any).QUnit.test;
-var assertOk = (globalThis as any).ok;
-var assertEqual = (globalThis as any).equal;
-var assertNotEqual = (globalThis as any).notEqual;
+var module = QUnit.module;
+var test = QUnit.test;
+var ok = QUnit.ok;
+var equal = QUnit.equal;
 
-moduleFn('URI.fragmentURI');
+module('URI.fragmentURI');
 
-testFn('storing URLs in fragment', function () {
+test('storing URLs in fragment', function () {
   var u = URI('http://example.org');
   var f: any;
 
@@ -24,48 +23,48 @@ testFn('storing URLs in fragment', function () {
   // furi.pathname('/hello.html');
   // uri.toString() === 'http://example.org/#!/hello.html'
 
-  assertOk(u.fragment(true) instanceof URI, 'URI instance for missing fragment');
+  ok(u.fragment(true) instanceof URI, 'URI instance for missing fragment');
 
   u = URI('http://example.org/#');
-  assertOk(u.fragment(true) instanceof URI, 'URI instance for empty fragment');
+  ok(u.fragment(true) instanceof URI, 'URI instance for empty fragment');
 
   u = URI('http://example.org/#!/foo/bar/baz.html');
   f = u.fragment(true);
-  assertEqual(f.pathname(), '/foo/bar/baz.html', 'reading path of FragmentURI');
-  assertEqual(f.filename(), 'baz.html', 'reading filename of FragmentURI');
+  equal(f.pathname(), '/foo/bar/baz.html', 'reading path of FragmentURI');
+  equal(f.filename(), 'baz.html', 'reading filename of FragmentURI');
 
   f.filename('foobar.txt');
-  assertEqual(f.pathname(), '/foo/bar/foobar.txt', 'modifying filename of FragmentURI');
-  assertEqual(u.fragment(), '!/foo/bar/foobar.txt', 'modifying fragment() through FragmentURI on original');
-  assertEqual(u.toString(), 'http://example.org/#!/foo/bar/foobar.txt', 'modifying filename of FragmentURI on original');
+  equal(f.pathname(), '/foo/bar/foobar.txt', 'modifying filename of FragmentURI');
+  equal(u.fragment(), '!/foo/bar/foobar.txt', 'modifying fragment() through FragmentURI on original');
+  equal(u.toString(), 'http://example.org/#!/foo/bar/foobar.txt', 'modifying filename of FragmentURI on original');
 });
 
-testFn('fragmentPrefix', function () {
+test('fragmentPrefix', function () {
   var u: any;
 
   (URI as any).fragmentPrefix = '?';
   u = URI('http://example.org');
-  assertEqual(u._parts.fragmentPrefix, '?', 'init using global property');
+  equal(u._parts.fragmentPrefix, '?', 'init using global property');
 
   u.fragment('#!/foo/bar/baz.html');
-  assertEqual(u.fragment(), '!/foo/bar/baz.html', 'unparsed ?');
-  assertOk(u.fragment(true) instanceof URI, 'parsing ? prefix - is URI');
-  assertEqual(u.fragment(true).toString(), '', 'parsing ? prefix - result');
+  equal(u.fragment(), '!/foo/bar/baz.html', 'unparsed ?');
+  ok(u.fragment(true) instanceof URI, 'parsing ? prefix - is URI');
+  equal(u.fragment(true).toString(), '', 'parsing ? prefix - result');
 
   u.fragment('#?/foo/bar/baz.html');
-  assertEqual(u.fragment(), '?/foo/bar/baz.html', 'unparsed ?');
-  assertOk(u.fragment(true) instanceof URI, 'parsing ? prefix - is URI');
-  assertEqual(u.fragment(true).toString(), '/foo/bar/baz.html', 'parsing ? prefix - result');
+  equal(u.fragment(), '?/foo/bar/baz.html', 'unparsed ?');
+  ok(u.fragment(true) instanceof URI, 'parsing ? prefix - is URI');
+  equal(u.fragment(true).toString(), '/foo/bar/baz.html', 'parsing ? prefix - result');
 
   u.fragmentPrefix('§');
-  assertEqual(u.fragment(), '?/foo/bar/baz.html', 'unparsed §');
-  assertOk(u.fragment(true) instanceof URI, 'parsing § prefix - is URI');
-  assertEqual(u.fragment(true).toString(), '', 'parsing § prefix - result');
+  equal(u.fragment(), '?/foo/bar/baz.html', 'unparsed §');
+  ok(u.fragment(true) instanceof URI, 'parsing § prefix - is URI');
+  equal(u.fragment(true).toString(), '', 'parsing § prefix - result');
 
   u.fragment('#§/foo/bar/baz.html');
-  assertEqual(u.fragment(), '§/foo/bar/baz.html', 'unparsed §');
-  assertOk(u.fragment(true) instanceof URI, 'parsing § prefix - is URI');
-  assertEqual(u.fragment(true).toString(), '/foo/bar/baz.html', 'parsing § prefix - result');
+  equal(u.fragment(), '§/foo/bar/baz.html', 'unparsed §');
+  ok(u.fragment(true) instanceof URI, 'parsing § prefix - is URI');
+  equal(u.fragment(true).toString(), '/foo/bar/baz.html', 'parsing § prefix - result');
 
   (URI as any).fragmentPrefix = '!';
 });
