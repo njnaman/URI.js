17f6d282162d90b3e23b862199a113295a4aabd5
naman.jain <naman.jain@media.net>
2025-08-22 00:42:47 +0530

    added rollup

--- typescript-migration/src/IPv6.ts
+++ typescript-migration/src/IPv6.ts
@@ -12,189 +12,144 @@
  *
  */
 
-// Define the IPv6 interface
-interface IPv6 {
-  best: (address: string) => string;
-  noConflict: () => IPv6;
+function bestPresentation(address: string): string {
+  // based on:
+  // Javascript to test an IPv6 address for proper format, and to
+  // present the "best text representation" according to IETF Draft RFC at
+  // http://tools.ietf.org/html/draft-ietf-6man-text-addr-representation-04
+  // 8 Feb 2010 Rich Brown, Dartware, LLC
+  // Please feel free to use this code as long as you provide a link to
+  // http://www.intermapper.com
+  // http://intermapper.com/support/tools/IPV6-Validator.aspx
+  // http://download.dartware.com/thirdparty/ipv6validator.js
+
+  const _address = address.toLowerCase();
+  const segments = _address.split(':');
+  let length = segments.length;
+  let total = 8;
+
+  // trim colons (:: or ::a:b:c… or …a:b:c::)
+  if (segments[0] === '' && segments[1] === '' && segments[2] === '') {
+    // must have been ::
+    // remove first two items
+    segments.shift();
+    segments.shift();
+  } else if (segments[0] === '' && segments[1] === '') {
+    // must have been ::xxxx
+    // remove the first item
+    segments.shift();
+  } else if (segments[length - 1] === '' && segments[length - 2] === '') {
+    // must have been xxxx::
+    segments.pop();
+  }
+
+  length = segments.length;
+
+  // adjust total segments for IPv4 trailer
+  if (segments[length - 1].indexOf('.') !== -1) {
+    // found a "." which means IPv4
+    total = 7;
+  }
+
+  // fill empty segments them with "0000"
+  let pos: number;
+  for (pos = 0; pos < length; pos++) {
+    if (segments[pos] === '') {
+      break;
+    }
+  }
+
+  if (pos < total) {
+    segments.splice(pos, 1, '0000');
+    while (segments.length < total) {
+      segments.splice(pos, 0, '0000');
+    }
+  }
+
+  // strip leading zeros
+  let _segments: string[];
+  for (let i = 0; i < total; i++) {
+    _segments = segments[i].split('');
+    for (let j = 0; j < 3; j++) {
+      if (_segments[0] === '0' && _segments.length > 1) {
+        _segments.splice(0, 1);
+      } else {
+        break;
+      }
+    }
+
+    segments[i] = _segments.join('');
+  }
+
+  // find longest sequence of zeroes and coalesce them into one segment
+  let best = -1;
+  let _best = 0;
+  let _current = 0;
+  let current = -1;
+  let inzeroes = false;
+  // i; already declared
+
+  let i: number;
+  for (i = 0; i < total; i++) {
+    if (inzeroes) {
+      if (segments[i] === '0') {
+        _current += 1;
+      } else {
+        inzeroes = false;
+        if (_current > _best) {
+          best = current;
+          _best = _current;
+        }
+      }
+    } else {
+      if (segments[i] === '0') {
+        inzeroes = true;
+        current = i;
+        _current = 1;
+      }
+    }
+  }
+
+  if (_current > _best) {
+    best = current;
+    _best = _current;
+  }
+
+  if (_best > 1) {
+    segments.splice(best, _best, '');
+  }
+
+  length = segments.length;
+
+  // assemble remaining segments
+  let result = '';
+  if (segments[0] === '') {
+    result = ':';
+  }
+
+  for (i = 0; i < length; i++) {
+    result += segments[i];
+    if (i === length - 1) {
+      break;
+    }
+
+    result += ':';
+  }
+
+  if (segments[length - 1] === '') {
+    result += ':';
+  }
+
+  return result;
 }
 
-// IPv6 will be declared by the UMD wrapper below
+function noConflict() {
+  return IPv6;
+}
 
-(function (root: any, factory: (root?: any) => IPv6) {
-  'use strict';
-  // https://github.com/umdjs/umd/blob/master/returnExports.js
-  if (typeof module === 'object' && module.exports) {
-    // Node
-    IPv6 = module.exports = factory();
-  } else if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(factory);
-  } else {
-    // Browser globals (root is window)
-    IPv6 = factory(root);
-    if (root) {
-      root.IPv6 = IPv6;
-    }
-  }
-}(this, function (root?: any): IPv6 {
-  'use strict';
+const IPv6 = {
+  best: bestPresentation,
+  noConflict: noConflict
+};
 
-  /*
-  var _in = "fe80:0000:0000:0000:0204:61ff:fe9d:f156";
-  var _out = IPv6.best(_in);
-  var _expected = "fe80::204:61ff:fe9d:f156";
-
-  console.log(_in, _out, _expected, _out === _expected);
-  */
-
-  // save current IPv6 variable, if any
-  const _IPv6 = root && root.IPv6;
-
-  function bestPresentation(address: string): string {
-    // based on:
-    // Javascript to test an IPv6 address for proper format, and to
-    // present the "best text representation" according to IETF Draft RFC at
-    // http://tools.ietf.org/html/draft-ietf-6man-text-addr-representation-04
-    // 8 Feb 2010 Rich Brown, Dartware, LLC
-    // Please feel free to use this code as long as you provide a link to
-    // http://www.intermapper.com
-    // http://intermapper.com/support/tools/IPV6-Validator.aspx
-    // http://download.dartware.com/thirdparty/ipv6validator.js
-
-    const _address = address.toLowerCase();
-    const segments = _address.split(':');
-    let length = segments.length;
-    let total = 8;
-
-    // trim colons (:: or ::a:b:c… or …a:b:c::)
-    if (segments[0] === '' && segments[1] === '' && segments[2] === '') {
-      // must have been ::
-      // remove first two items
-      segments.shift();
-      segments.shift();
-    } else if (segments[0] === '' && segments[1] === '') {
-      // must have been ::xxxx
-      // remove the first item
-      segments.shift();
-    } else if (segments[length - 1] === '' && segments[length - 2] === '') {
-      // must have been xxxx::
-      segments.pop();
-    }
-
-    length = segments.length;
-
-    // adjust total segments for IPv4 trailer
-    if (segments[length - 1].indexOf('.') !== -1) {
-      // found a "." which means IPv4
-      total = 7;
-    }
-
-    // fill empty segments them with "0000"
-    let pos: number;
-    for (pos = 0; pos < length; pos++) {
-      if (segments[pos] === '') {
-        break;
-      }
-    }
-
-    if (pos < total) {
-      segments.splice(pos, 1, '0000');
-      while (segments.length < total) {
-        segments.splice(pos, 0, '0000');
-      }
-    }
-
-    // strip leading zeros
-    let _segments: string[];
-    for (let i = 0; i < total; i++) {
-      _segments = segments[i].split('');
-      for (let j = 0; j < 3 ; j++) {
-        if (_segments[0] === '0' && _segments.length > 1) {
-          _segments.splice(0,1);
-        } else {
-          break;
-        }
-      }
-
-      segments[i] = _segments.join('');
-    }
-
-    // find longest sequence of zeroes and coalesce them into one segment
-    let best = -1;
-    let _best = 0;
-    let _current = 0;
-    let current = -1;
-    let inzeroes = false;
-    // i; already declared
-
-    let i: number;
-    for (i = 0; i < total; i++) {
-      if (inzeroes) {
-        if (segments[i] === '0') {
-          _current += 1;
-        } else {
-          inzeroes = false;
-          if (_current > _best) {
-            best = current;
-            _best = _current;
-          }
-        }
-      } else {
-        if (segments[i] === '0') {
-          inzeroes = true;
-          current = i;
-          _current = 1;
-        }
-      }
-    }
-
-    if (_current > _best) {
-      best = current;
-      _best = _current;
-    }
-
-    if (_best > 1) {
-      segments.splice(best, _best, '');
-    }
-
-    length = segments.length;
-
-    // assemble remaining segments
-    let result = '';
-    if (segments[0] === '')  {
-      result = ':';
-    }
-
-    for (i = 0; i < length; i++) {
-      result += segments[i];
-      if (i === length - 1) {
-        break;
-      }
-
-      result += ':';
-    }
-
-    if (segments[length - 1] === '') {
-      result += ':';
-    }
-
-    return result;
-  }
-
-  function noConflict(): IPv6 {
-    /*jshint validthis: true */
-    if (root && root.IPv6 === this) {
-      root.IPv6 = _IPv6;
-    }
-
-    return this;
-  }
-
-  return {
-    best: bestPresentation,
-    noConflict: noConflict
-  };
-}));
-
-// ES6 export for TypeScript
-// IPv6 is exported via UMD wrapper above
+export default IPv6;
