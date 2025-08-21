50bd2163a7bd2d980bb2ef2663acc01d54c6899d
naman.jain <naman.jain@media.net>
2025-08-22 00:57:21 +0530

    added SecondLevelDomains

--- typescript-migration/rollup.config.js
+++ typescript-migration/rollup.config.js
@@ -1,8 +1,21 @@
-export default {
-    input: 'dist/IPv6.js',
-    output: {
-        file: 'dist/IPv6.js',
-        format: 'umd',
-        name: 'IPv6'
-    }
-};
+export default [
+    {
+        input: 'dist/src/IPv6.js',
+        output: {
+            file: 'dist/src/IPv6.js',
+            format: 'umd',
+            name: 'IPv6'
+        },
+    },
+
+    {
+        input: 'dist/src/SecondLevelDomains.js',
+        output: {
+            file: 'dist/src/SecondLevelDomains.js',
+            format: 'umd',
+            name: 'SecondLevelDomains'
+        },
+    },
+
+
+];
