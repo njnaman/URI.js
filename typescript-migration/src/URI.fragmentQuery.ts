17f89cb12655c25302f477a413e1f7d27dbab861
naman.jain <naman.jain@media.net>
2025-08-23 12:20:48 +0530

    added changes in fragment query

--- typescript-migration/src/URI.fragmentQuery.ts
+++ typescript-migration/src/URI.fragmentQuery.ts
@@ -24,128 +24,86 @@
 // uri.setFragment("name", "value2");
 // uri.toString() === "http://example.org/#?bar=foo&name=value2";
 
-// Declare URI variable at module level
-var URIFragmentQuery: any;
+const p = URI.prototype;
+// old fragment handler we need to wrap
+const f = p.fragment;
 
-// Extended interface for fragment query functionality
-interface URIFragmentQueryExtended {
-  fragmentPrefix(prefix: string): any;
-  fragment(returnParsed: true): any;
-  fragment(data: any): any;
-  addFragment(name: string, value: string | null, build?: boolean): any;
-  addFragment(data: any, build?: boolean): any;
-  removeFragment(name: string, value?: string, build?: boolean): any;
-  removeFragment(data: any, build?: boolean): any;
-  setFragment(name: string, value: string | null, build?: boolean): any;
-  setFragment(data: any, build?: boolean): any;
-  addHash(name: string, value: string | null, build?: boolean): any;
-  addHash(data: any, build?: boolean): any;
-  removeHash(name: string, value?: string, build?: boolean): any;
-  removeHash(data: any, build?: boolean): any;
-  setHash(name: string, value: string | null, build?: boolean): any;
-  setHash(data: any, build?: boolean): any;
-}
+// make fragmentPrefix configurable
+URI.fragmentPrefix = '?';
+const _parts = URI._parts;
+URI._parts = function(): any {
+  const parts = _parts();
+  parts.fragmentPrefix = URI.fragmentPrefix;
+  return parts;
+};
 
-(function (root: any, factory) {
-  'use strict';
-  // https://github.com/umdjs/umd/blob/master/returnExports.js
-  if (typeof module === 'object' && module.exports) {
-    // Node
-    URIFragmentQuery = module.exports = factory(require('./URI'));
-  } else if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(['./URI'], factory);
+p.fragmentPrefix = function(v: string): any {
+  this._parts.fragmentPrefix = v;
+  return this;
+};
+
+// add fragment(true) and fragment({key: value}) signatures
+p.fragment = function(v?: any, build?: boolean): any {
+  const prefix = this._parts.fragmentPrefix;
+  const fragment = this._parts.fragment || '';
+
+  if (v === true) {
+    if (fragment.substring(0, prefix.length) !== prefix) {
+      return {};
+    }
+
+    return URI.parseQuery(fragment.substring(prefix.length));
+  } else if (v !== undefined && typeof v !== 'string') {
+    this._parts.fragment = prefix + URI.buildQuery(v);
+    this.build(!build);
+    return this;
   } else {
-    // Browser globals (root is window)
-    factory((root as any)?.URI);
+    return f.call(this, v, build);
   }
-}(this, function (URI: any) {
-  'use strict';
+};
 
-  const p = URI.prototype;
-  // old fragment handler we need to wrap
-  const f = p.fragment;
+p.addFragment = function(name: any, value?: any, build?: boolean): any {
+  const prefix = this._parts.fragmentPrefix;
+  const data = URI.parseQuery((this._parts.fragment || '').substring(prefix.length));
+  URI.addQuery(data, name, value);
+  this._parts.fragment = prefix + URI.buildQuery(data);
+  if (typeof name !== 'string') {
+    build = value;
+  }
 
-  // make fragmentPrefix configurable
-  URI.fragmentPrefix = '?';
-  const _parts = URI._parts;
-  URI._parts = function(): any {
-    const parts = _parts();
-    parts.fragmentPrefix = URI.fragmentPrefix;
-    return parts;
-  };
+  this.build(!build);
+  return this;
+};
 
-  p.fragmentPrefix = function(v: string): any {
-    this._parts.fragmentPrefix = v;
-    return this;
-  };
+p.removeFragment = function(name: any, value?: any, build?: boolean): any {
+  const prefix = this._parts.fragmentPrefix;
+  const data = URI.parseQuery((this._parts.fragment || '').substring(prefix.length));
+  URI.removeQuery(data, name, value);
+  this._parts.fragment = prefix + URI.buildQuery(data);
+  if (typeof name !== 'string') {
+    build = value;
+  }
 
-  // add fragment(true) and fragment({key: value}) signatures
-  p.fragment = function(v?: any, build?: boolean): any {
-    const prefix = this._parts.fragmentPrefix;
-    const fragment = this._parts.fragment || '';
+  this.build(!build);
+  return this;
+};
 
-    if (v === true) {
-      if (fragment.substring(0, prefix.length) !== prefix) {
-        return {};
-      }
+p.setFragment = function(name: any, value?: any, build?: boolean): any {
+  const prefix = this._parts.fragmentPrefix;
+  const data = URI.parseQuery((this._parts.fragment || '').substring(prefix.length));
+  URI.setQuery(data, name, value);
+  this._parts.fragment = prefix + URI.buildQuery(data);
+  if (typeof name !== 'string') {
+    build = value;
+  }
 
-      return URI.parseQuery(fragment.substring(prefix.length));
-    } else if (v !== undefined && typeof v !== 'string') {
-      this._parts.fragment = prefix + URI.buildQuery(v);
-      this.build(!build);
-      return this;
-    } else {
-      return f.call(this, v, build);
-    }
-  };
+  this.build(!build);
+  return this;
+};
 
-  p.addFragment = function(name: any, value?: any, build?: boolean): any {
-    const prefix = this._parts.fragmentPrefix;
-    const data = URI.parseQuery((this._parts.fragment || '').substring(prefix.length));
-    URI.addQuery(data, name, value);
-    this._parts.fragment = prefix + URI.buildQuery(data);
-    if (typeof name !== 'string') {
-      build = value;
-    }
+p.addHash = p.addFragment;
+p.removeHash = p.removeFragment;
+p.setHash = p.setFragment;
 
-    this.build(!build);
-    return this;
-  };
-
-  p.removeFragment = function(name: any, value?: any, build?: boolean): any {
-    const prefix = this._parts.fragmentPrefix;
-    const data = URI.parseQuery((this._parts.fragment || '').substring(prefix.length));
-    URI.removeQuery(data, name, value);
-    this._parts.fragment = prefix + URI.buildQuery(data);
-    if (typeof name !== 'string') {
-      build = value;
-    }
-
-    this.build(!build);
-    return this;
-  };
-
-  p.setFragment = function(name: any, value?: any, build?: boolean): any {
-    const prefix = this._parts.fragmentPrefix;
-    const data = URI.parseQuery((this._parts.fragment || '').substring(prefix.length));
-    URI.setQuery(data, name, value);
-    this._parts.fragment = prefix + URI.buildQuery(data);
-    if (typeof name !== 'string') {
-      build = value;
-    }
-
-    this.build(!build);
-    return this;
-  };
-
-  p.addHash = p.addFragment;
-  p.removeHash = p.removeFragment;
-  p.setHash = p.setFragment;
-
-  // extending existing object rather than defining something new
-  return URI;
-}));
-
-// ES6 export for TypeScript
-// URI is exported via UMD wrapper above
+// extending existing object rather than defining something new
+export default URI;
